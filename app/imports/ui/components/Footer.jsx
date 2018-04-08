bdf34adc0f8fd01965c2c9015c083284351e5aa9
arnoldshek <ashek@hawaii.edu>
2018-04-07 15:57:19 -1000

    Revised text for footer, changed footer color

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', color: 'white' };
     return (
         <footer>
           <div style={divStyle} className="ui center aligned container">
             <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
+            Hale Manoa ICS 314 <br />
+            University of Hawaii<br />
+            Honolulu, HI 96822<br />
+            Github Development Page: <a href="https://hale-manoa.github.io/">
+            https://hale-manoa.github.io/</a>
           </div>
         </footer>
     );
