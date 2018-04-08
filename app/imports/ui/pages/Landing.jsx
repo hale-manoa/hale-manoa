8050af6ec58852a0441df395b7605291795fbfd1
kyle-chan <chankyle@hawaii.edu>
2018-04-07 16:40:25 -1000

    Ported digits database

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,12 +8,12 @@ class Landing extends React.Component {
         <Grid verticalAlign='middle' textAlign='center' container>
 
           <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+            <Image size='small' src="/images/halemanoa.png"/>
           </Grid.Column>
 
           <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+            <h1>Issue-3</h1>
+            <p>Implement User Bios. Sign in and select View Bios.</p>
           </Grid.Column>
 
         </Grid>
