b8f83316ac998b5086fdc9891fe3128d1f86ec22
arnoldshek <ashek@hawaii.edu>
2018-04-17 09:26:37 -1000

    Modified a bit

--- app/imports/ui/pages/ContactUs.jsx
+++ app/imports/ui/pages/ContactUs.jsx
@@ -5,40 +5,42 @@ import { Grid, List } from 'semantic-ui-react';
 export default class ContactUs extends React.Component {
   render() {
     return (
-        <Grid container columns="four">
-          <Grid.Column>
-            Kyle Chan
-            <hr/>
-            <List>
-              <a href="https://kyle-chan.github.io/">
-                https://kyle-chan.github.io/</a>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            Jonathan Lau
-            <hr/>
-            <List>
-              <a href="https://jon-lau.github.io/">
-                https://jon-lau.github.io/</a>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            Arnold Shek
-            <hr/>
-            <List>
-              <a href="https://arnoldshek.github.io/">
-                https://arnoldshek.github.io/</a>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            Akira Vernon
-            <hr/>
-            <List>
-              <a href="https://akirav.github.io/">
-                https://akirav.github.io/</a>
-            </List>
-          </Grid.Column>
-        </Grid>
+        <div className="contact-us-container">
+          <Grid container columns="four">
+            <Grid.Column className="contact-us-font">
+              Kyle Chan
+              <hr/>
+              <List>
+                <a href="https://kyle-chan.github.io/">
+                  https://kyle-chan.github.io/</a>
+              </List>
+            </Grid.Column>
+            <Grid.Column className="contact-us-font">
+              Jonathan Lau
+              <hr/>
+              <List>
+                <a href="https://jon-lau.github.io/">
+                  https://jon-lau.github.io/</a>
+              </List>
+            </Grid.Column>
+            <Grid.Column className="contact-us-font">
+              Arnold Shek
+              <hr/>
+              <List>
+                <a href="https://arnoldshek.github.io/">
+                  https://arnoldshek.github.io/</a>
+              </List>
+            </Grid.Column>
+            <Grid.Column className="contact-us-font">
+              Akira Vernon
+              <hr/>
+              <List>
+                <a href="https://akirav.github.io/">
+                  https://akirav.github.io/</a>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
