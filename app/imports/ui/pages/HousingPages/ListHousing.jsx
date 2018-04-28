e92a0c0cc5d5e837b5b2bd777344c558d7b7e217
kyle-chan <chankyle@hawaii.edu>
2018-04-28 11:30:16 -1000

    Property type and city filters working. Need to fix bed/bath filters

--- app/imports/ui/pages/HousingPages/ListHousing.jsx
+++ app/imports/ui/pages/HousingPages/ListHousing.jsx
@@ -1,17 +1,122 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Card, Header, Loader } from 'semantic-ui-react';
+import { Container, Card, Header, Loader, Modal, Button, Icon, Dropdown, Grid } from 'semantic-ui-react';
 import { Housings } from '/imports/api/housing/housing';
 import HousingItem from '/imports/ui/components/HousingItem';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 
+const property_type = [
+  { key: 'Condos', text: 'Condos', value: 'CONDO' },
+  { key: 'Houses', text: 'Houses', value: 'HOUSE' },
+  { key: 'Townhouses', text: 'Townhouses', value: 'TOWNHOUSE' },
+];
+
+const list_city = [
+  { key: 'Honolulu', text: 'Honolulu', value: 'Honolulu' },
+  { key: 'Waipahu', text: 'Waipahu', value: 'Waipahu' },
+];
+
+const num_bed = [
+  { key: '1', text: '1', value: '1' },
+  { key: '2', text: '2', value: '2' },
+];
+
+const num_bath = [
+  { key: '1', text: '1', value: '1' },
+];
+
+const sq_ft = [
+  { key: '300', text: '300', value: '300' },
+  { key: '500', text: '500', value: '500' },
+];
+
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListHousing extends React.Component {
+  constructor() {
+    super();
+    this.handleTypeSubmit = this.handleTypeSubmit.bind(this);
+    this.handleCitySubmit = this.handleCitySubmit.bind(this);
+    this.handleBedSubmit = this.handleBedSubmit.bind(this);
+    this.handleBathSubmit = this.handleBathSubmit.bind(this);
+    this.handleAreaSubmit = this.handleAreaSubmit.bind(this);
+    this.handleApply = this.handleApply.bind(this);
+    this.updateModalStateOpen = this.updateModalStateOpen.bind(this);
+    this.updateModalStateClose = this.updateModalStateClose.bind(this);
+    this.state = {
+      tentativeType: [],
+      tentativeCity: [],
+      tentativeBed: [],
+      tentativeBath: [],
+      tentativeArea: [],
+      selectedType: [],
+      selectedCity: [],
+      selectedBed: [],
+      selectedBath: [],
+      selectedArea: [],
+      modalOpen: false,
+    };
+  }
+  handleTypeSubmit(event, data) {
+    event.preventDefault();
+    this.setState({ tentativeType: data.value });
+  }
 
+  handleCitySubmit(event, data) {
+    event.preventDefault();
+    this.setState({ tentativeCity: data.value });
+  }
+
+  handleBedSubmit(event, data) {
+    event.preventDefault();
+    this.setState({ tentativeBed: data.value });
+  }
+
+  handleBathSubmit(event, data) {
+    event.preventDefault();
+    this.setState({ tentativeBath: data.value });
+  }
+
+  handleAreaSubmit(event, data) {
+    event.preventDefault();
+    this.setState({ tentativeArea: data.value });
+  }
+
+  handleApply(event) {
+    event.preventDefault();
+    this.setState({ selectedType: this.state.tentativeType });
+    this.setState({ selectedCity: this.state.tentativeCity });
+    this.setState({ selectedBed: this.state.tentativeBed });
+    this.setState({ selectedBath: this.state.tentativeBath });
+    this.setState({ selectedArea: this.state.tentativeArea });
+    this.setState({ modalOpen: false });
+  }
+
+  updateModalStateOpen(event) {
+    event.preventDefault();
+    this.setState({ modalOpen: true });
+  }
+
+  updateModalStateClose(event) {
+    event.preventDefault();
+    this.setState({ modalOpen: false });
+  }
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
+  }
+
+  filterListingsbyType() {
+    const type = this.state.selectedType;
+    const city = this.state.selectedCity;
+    const bed = this.state.selectedBed;
+    const bath = this.state.selectedBath;
+    const area = this.state.selectedArea;
+    return this.props.housings.filter(m => type.length === 0 || (type.indexOf(m.propertytype) !== -1))
+        .filter(m => city.length === 0 || (city.indexOf(m.city) !== -1));
+        /** .filter(m => bed.length === 0 || (bed.indexOf(m.bed) !== -1));
+        .filter(m => bath.length === 0 || (bath.indexOf(m.bath) !== -1))
+        .filter(m => area.length === 0 || (area.indexOf(m.area) !== -1)); */
   }
 
   /** Render the page once subscriptions have been received. */
@@ -19,8 +124,88 @@ class ListHousing extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center">Housing</Header>
+          <Modal size='large' open={ this.state.modalOpen } onClose={this.updateModalStateClose}
+                 trigger={<Button onClick={this.updateModalStateOpen}>Filter Listings By</Button>
+                 } closeIcon>
+            <Header icon='filter' content='Filters' />
+            <Modal.Content>
+              <Grid columns={5}>
+                <Grid.Column>
+                  <p>
+                    User Type
+                    <Dropdown
+                        multiple selection
+                        button
+                        options={property_type}
+                        placeholder='Select Property Type'
+                        value={this.state.tentativeType}
+                        onChange={this.handleTypeSubmit}
+                    />
+                  </p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>
+                    Age
+                    <Dropdown
+                        multiple selection
+                        button
+                        options={list_city}
+                        placeholder='Select City'
+                        value={this.state.tentativeCity}
+                        onChange={this.handleCitySubmit}
+                    />
+                  </p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>
+                    Area
+                    <Dropdown
+                        multiple selection
+                        button
+                        options={num_bed}
+                        placeholder='Number of Bedrooms'
+                        value={this.state.tentativeBed}
+                        onChange={this.handleBedSubmit}
+                    />
+                  </p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>
+                    Preferences
+                    <Dropdown
+                        multiple selection
+                        button
+                        options={num_bath}
+                        placeholder='Number of Bathrooms'
+                        value={this.state.tentativeBath}
+                        onChange={this.handleBathSubmit}
+                    />
+                  </p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>
+                    User Type
+                    <Dropdown
+                        multiple selection
+                        button
+                        options={sq_ft}
+                        placeholder='Area (sq. ft.)'
+                        value={this.state.tentativeArea}
+                        onChange={this.handleAreaSubmit}
+                    />
+                  </p>
+                </Grid.Column>
+              </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='green' onClick={this.handleApply}>
+                <Icon name='checkmark' /> Apply
+              </Button>
+            </Modal.Actions>
+          </Modal>
           <Card.Group centered>
-            {this.props.housings.map((house, index) => <HousingItem key={index} house={house}/>)}
+            { /** {this.props.housings.map((house, index) => <HousingItem key={index} house={house}/>)} */ }
+            {this.filterListingsbyType().map((house, index) => <HousingItem key ={index} house={house}/>)}
           </Card.Group>
         </Container>
     );
