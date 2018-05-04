4c48177c279a5af067127df34253db68f40bf23e
Akira Vernon <akirav@hawaii.edu>
2018-05-04 10:10:13 -1000

    Fixed Edit Housing

--- app/imports/ui/pages/HousingPages/EditHousing.jsx
+++ app/imports/ui/pages/HousingPages/EditHousing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Loader, Header, Container } from 'semantic-ui-react';
+import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
 import { Housings, HousingsSchema } from '/imports/api/housing/housing';
 import { Bert } from 'meteor/themeteorchef:bert';
 import AutoForm from 'uniforms-semantic/AutoForm';
@@ -13,16 +13,100 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import '/imports/ui/pages/HousingPages/listhousingpage.css';
+import { compose, withProps, lifecycle } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox';
+
+
+let adrs = 'please';
+const loc = { lat: 0, lng: 0 };
+let parsed = [];
+let zipcode = [];
+
+const SearchBox = compose(
+    withProps({
+      googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBxxQ6xRUt4VMU4EC5fVxJE5ah5aLa0D7k&v=3.exp&libraries=geometry,drawing,places',
+      loadingElement: <div style={{ height: '100%' }}/>,
+      containerElement: <div style={{ height: '400px' }}/>,
+    }),
+
+
+    lifecycle({
+      componentWillMount() {
+        const refs = {};
+        this.setState({
+          places: [],
+          onSearchBoxMounted: ref => {
+            refs.searchBox = ref;
+          },
+          onPlacesChanged: () => {
+            const places = refs.searchBox.getPlaces();
+            this.setState({
+              places,
+            });
+          },
+        });
+      },
+    }),
+    withScriptjs,
+)(props =>
+    <div data-standalone-searchbox="">
+      <StandaloneSearchBox
+          ref={props.onSearchBoxMounted}
+          bounds={props.bounds}
+          onPlacesChanged={props.onPlacesChanged}
+      >
+        <input
+            type="text"
+            placeholder=" "
+            style={{
+              boxSizing: 'border-box',
+              height: '38px',
+              padding: '0 12px',
+              borderRadius: '3px',
+              fontSize: '14px',
+              outline: 'none',
+              textOverflow: 'ellipses',
+            }}
+        />
+      </StandaloneSearchBox>
+      <ol>
+        {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
+            <ol key={place_id}>
+              {adrs = formatted_address}
+              {' at '}
+              ({loc.lat = location.lat()}, {loc.lng = location.lng()})
+              {props.onChange(adrs)}
+            </ol>)}
+      </ol>
+    </div>);
 
 /** Renders the Page for editing a single document. */
 class EditHousing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onAddressChange = this.onAddressChange.bind(this);
+    this.formRef = null;
+    this.state = {
+      address: 'State Test',
+      lat: 0,
+      lng: 0,
+    };
+  }
+
+
+  onAddressChange(address) {
+    this.setState( { address: address, lat: loc.lat, lng: loc.lng  } );
+    console.log('Changed State');
+    parsed = address.split(', ');
+    zipcode = parsed[2].split(' ');
+  }
 
   /** On successful submit, insert the data. */
   submit(data) {
-    const { streetaddress, unitnumber, city, state, image, zipcode, propertytype, rentprice, beds, baths, squarefeet, description, _id } = data;
+    const { streetaddress, unitnumber, city, state, image, zipcode, propertytype, rentprice, beds, baths, squarefeet, description, longitude, latitude, _id } = data;
     Housings.update(_id, {
       $set: {
-        streetaddress,
         unitnumber,
         city,
         state,
@@ -33,7 +117,10 @@ class EditHousing extends React.Component {
         beds,
         baths,
         squarefeet,
-        description
+        description,
+        streetaddress,
+        longitude,
+        latitude,
       }
     }, (error) => (error ?
         Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` }) :
@@ -48,70 +135,42 @@ class EditHousing extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   renderPage() {
     return (
-        <Grid centered>
-          <Grid.Column width={6}>
-            <Container className="pageContainer">
-              <Header as="h2" textAlign="center">Edit Housing</Header>
-              <AutoForm ref={(ref) => {
-                this.formRef = ref;
-              }} schema={HousingsSchema} onSubmit={this.submit} model={this.props.doc}>
-                <Grid centered>
-                  <Grid.Row>
-                    <Grid.Column width={8}>
-                      <TextField name='streetaddress'/>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <TextField name='unitnumber'/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Column width={4}>
-                      <TextField name='city'/>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <TextField name='state'/>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <NumField name='zipcode' decimal={false}/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Column width={5}>
-                      <SelectField name='propertytype'/>
-                    </Grid.Column>
-                    <Grid.Column width={9}>
-                      <TextField name='image'/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Column width={4}>
-                      <NumField name='rentprice' decimal={false}/>
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                      <NumField name='beds' decimal={false}/>
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                      <NumField name='baths' decimal={false}/>
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                      <NumField name='squarefeet' decimal={false}/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Column width={14}>
-                      <TextField name='description'/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <Grid.Row>
-                    <Grid.Column width={6} className="button-spacing-2">
-                      <SubmitField value='Submit'/>
-                    </Grid.Column>
-                  </Grid.Row>
-                  <ErrorsField/>
-                  <HiddenField name='owner' value='fakeyser@foo.com'/>
-                </Grid>
-              </AutoForm>
-            </Container>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Add Housing</Header>
+            <AutoForm ref={(ref) => {
+              this.formRef = ref;
+            }} schema={HousingsSchema} onSubmit={this.submit} model={this.props.doc}>
+              <Segment>
+                <p style={{
+                  display: 'block',
+                  margin: '0em 0em 0.28571429rem 0em',
+                  color: 'rgba(0, 0, 0, 0.87)',
+                  fontSize: '0.92857143em',
+                  fontWeight: 'bold',
+                  textTransform: 'none',
+                }}>Address</p>
+                <SearchBox onChange={this.onAddressChange}/>
+                <TextField name='unitnumber'/>
+                <TextField name='image'/>
+                <SelectField name='propertytype'/>
+                <NumField name='rentprice' decimal={false}/>
+                <NumField name='beds' decimal={false}/>
+                <NumField name='baths' decimal={false}/>
+                <NumField name='squarefeet' decimal={false}/>
+                <TextField name='description'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <HiddenField name='owner' value='fakeyser@foo.com'/>
+                <HiddenField name='streetaddress' value={parsed[0]}/>
+                <HiddenField name='longitude' decimal={true} value={loc.lng}/>
+                <HiddenField name='latitude' decimal={true} value={loc.lat}/>
+                <HiddenField name='city' value={parsed[1]} />
+                <HiddenField name='state' value={zipcode[0]}/>
+                <HiddenField name='zipcode' decimal={false} value={zipcode[1]}/>
+
+              </Segment>
+            </AutoForm>
           </Grid.Column>
         </Grid>
     );
